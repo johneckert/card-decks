f396ede4b2a1fbe2182a6094e4da87057180c5ae
John Eckert <johnteckert@gmail.com>
2024-03-17 22:05:01 -0400

    fix image paths

--- src/Card.tsx
+++ src/Card.tsx
@@ -21,7 +21,7 @@ const Card: React.FC<CardProps> = ({ title, cardDrawn, setCardDrawn }) => {
         <div onClick={flipCard} className={`rounded-2xl border-4 border-solid border-black h-60 w-40 flex m-0.5 ${isFlipped ? "bg-white" : "bg-gradient-to-r from-red-800 via-red-600 to-red-800"}`}>
           { isFlipped ? (
             <div className='relative flex justify-center items-center overflow-hidden'>
-              <img className="object-cover w-full p-4 pb-8" src={`deckOfIllusions/${imageName}.png`} alt={title} />
+              <img className="object-cover w-full p-4 pb-8" src={`card-decks/deckOfIllusions/${imageName}.png`} alt={title} />
               <p className="absolute px-2 bottom-0 text-center align-text-bottom text-sm">{ title }</p>
             </div>
            ) : (
