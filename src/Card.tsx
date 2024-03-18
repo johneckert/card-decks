de453a27bea5fb051fa5a9c0fb1c2b5f2ff93485
John Eckert <johnteckert@gmail.com>
2024-03-17 22:45:45 -0400

    setup env vars for file path difference local v prod

--- src/Card.tsx
+++ src/Card.tsx
@@ -10,6 +10,7 @@ export interface CardProps extends CardData {
 const Card: React.FC<CardProps> = ({ title, cardDrawn, setCardDrawn }) => {
     const [isFlipped, setIsFlipped] = useState<boolean>(false);
     const imageName = title.toLowerCase().replace(/ /g, '-');
+    const isProd = process.env.NODE_ENV === 'production';
     const flipCard = () => {
       if (!cardDrawn) {
         setIsFlipped(true);
@@ -21,7 +22,7 @@ const Card: React.FC<CardProps> = ({ title, cardDrawn, setCardDrawn }) => {
         <div onClick={flipCard} className={`rounded-2xl border-4 border-solid border-black h-60 w-40 flex m-0.5 ${isFlipped ? "bg-white" : "bg-gradient-to-r from-red-800 via-red-600 to-red-800"}`}>
           { isFlipped ? (
             <div className='relative flex justify-center items-center overflow-hidden'>
-              <img className="object-cover w-full p-4 pb-8" src={`card-decks/deckOfIllusions/${imageName}.png`} alt={title} />
+              <img className="object-cover w-full p-4 pb-8" src={`${isProd ? '' : 'card-decks/'}deckOfIllusions/${imageName}.png`} alt={title} />
               <p className="absolute px-2 bottom-0 text-center align-text-bottom text-sm">{ title }</p>
             </div>
            ) : (
