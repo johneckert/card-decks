3a0b4bce26a617ca02a594d9e13da33fadea6c03
John Eckert <johnteckert@gmail.com>
2024-03-18 18:35:34 -0400

    fix typo from debugging

--- src/Card.tsx
+++ src/Card.tsx
@@ -19,13 +19,13 @@ const Card: React.FC<CardProps> = ({ title, cardDrawn, setCardDrawn }) => {
     };
 
     return (
-      <div onClick={flipCard} className={`rounded-2xl border-4 border-solid border-black h-60 w-40 flex m-0.5 ${isFlipped ? "bg-white" : "bg-gradient-to-r from-red-800 via-red-600 to-red-800"}`}>
-          <div className={`${isFlipped ? '' : 'hidden'} relative flex justify-center items-center overflow-hidden`}>
-            <img className="object-cover w-full p-4 pb-8" src={`${isProd ? '' : 'card-decks/'}deckOfIllusions/${imageName}.png`} alt={title} />
-            <p className="absolute px-2 bottom-0 text-center align-text-bottom text-sm">{ title }</p>
-          </div>
-          <img className={`${isFlipped ? 'hidden' : ''} w-100`} src={CardBack} alt="card back" />
-      </div>
+        <div onClick={flipCard} className={`rounded-2xl border-4 border-solid border-black h-60 w-40 flex m-0.5 ${isFlipped ? "bg-white" : "bg-gradient-to-r from-red-800 via-red-600 to-red-800"}`}>
+            <div className={`${isFlipped ? '' : 'hidden'} relative flex justify-center items-center overflow-hidden`}>
+              <img className="object-cover w-full p-4 pb-8" src={`${isProd ? '' : 'card-decks/'}deckOfIllusions/${imageName}.png`} alt={title} />
+              <p className="absolute px-2 bottom-0 text-center align-text-bottom text-sm">{ title }</p>
+            </div>
+            <img className={`${isFlipped ? 'hidden' : ''} w-100`} src={CardBack} alt="card back" />
+        </div>
     );
 }
 
